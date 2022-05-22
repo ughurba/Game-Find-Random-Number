e4a797d92a5146a6fe1a20e3adf18e46c9eb50e9
ughurba <ughurba@edu.code.az>
2022-05-23 01:30:35 +0400

    I added game

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Game-Find-Random-Number" 
---
+++ script.js
@@ -0,0 +1,77 @@
+let randomNumber = Math.trunc(Math.random() * 20 + 1);
+let highScore = 0;
+let score = 20;
+
+
+document.querySelector('.check').addEventListener('click', function() {
+  let numberInput = Number(document.querySelector('.number-input').value);
+  if (!numberInput) {
+    document.querySelector('.guess-message')
+      .textContent = 'Enter the number';
+  } else if (numberInput === randomNumber) {
+
+    document.querySelector('.guess-message')
+      .textContent = 'Nice bro';
+    document.querySelector('body')
+      .style
+      .backgroundColor = 'Green';
+    document.querySelector('.question')
+      .style
+      .width = '500px';
+    document.querySelector('.question')
+      .textContent = randomNumber;
+
+    if(score > highScore){
+      highScore = score
+      document.querySelector('.highscore')
+        .textContent = highScore;
+    }
+
+
+  } else if (numberInput < randomNumber) {
+
+    if (score > 1) {
+      document.querySelector('.guess-message')
+        .textContent = 'Too little';
+      score--;
+      document.querySelector('.score').textContent = score;
+
+    } else {
+
+      document.querySelector('.guess-message')
+        .textContent = 'Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+
+
+  } else if (numberInput > randomNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message')
+        .textContent = 'Too much';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+
+      document.querySelector('.guess-message')
+        .textContent = 'Game Over';
+      document.querySelector('.score').textContent = 0;
+    }
+
+
+  }
+
+});
+
+// yeniden bawlayanda
+
+document.querySelector('.again').addEventListener('click', function() {
+  document.querySelector('body').style.backgroundColor = 'black';
+  document.querySelector('.guess-message')
+    .textContent = 'start guessing';
+  document.querySelector('.question').style.width = '25rem';
+   randomNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.question').textContent = '???';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number-input').value = '';
+});
