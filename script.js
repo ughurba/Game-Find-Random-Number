773951e64fc5ac2496c5141be623734aed66dd6d
ughurba <ughurba@edu.code.az>
2022-05-23 02:44:36 +0400

    refactor:made the game responsive with media queries

--- script.js
+++ script.js
@@ -2,62 +2,74 @@ let randomNumber = Math.trunc(Math.random() * 20 + 1);
 let highScore = 0;
 let score = 20;
 
+//functions
+
+let displayGuessMessage = function(message){
+  document.querySelector('.guess-message').textContent = message;
+}
+
+let bodyBackgroundColor = function(color){
+  document.querySelector('body').style.backgroundColor = color;
+}
+
+let questionSize = function(width){
+  document.querySelector('.question').style.width = width;
+}
+
+let scoreSize = function(score){
+  document.querySelector('.score').textContent = score;
+}
+
+
+
+
 
 document.querySelector('.check').addEventListener('click', function() {
   let numberInput = Number(document.querySelector('.number-input').value);
   if (!numberInput) {
-    document.querySelector('.guess-message')
-      .textContent = 'Enter the number';
+
+    document.querySelector('.guess-message').textContent = 'Enter the number';
+
   } else if (numberInput === randomNumber) {
 
-    document.querySelector('.guess-message')
-      .textContent = 'Nice bro';
-    document.querySelector('body')
-      .style
-      .backgroundColor = 'Green';
-    document.querySelector('.question')
-      .style
-      .width = '500px';
-    document.querySelector('.question')
-      .textContent = randomNumber;
+    displayGuessMessage('Nice bro') ;
+    bodyBackgroundColor('Green') ;
+    questionSize('500px');
 
-    if(score > highScore){
-      highScore = score
-      document.querySelector('.highscore')
-        .textContent = highScore;
+    document.querySelector('.question').textContent = randomNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
     }
 
 
-  }else if (numberInput !== randomNumber){
+  } else if (numberInput !== randomNumber) {
 
     if (score > 1) {
-      document.querySelector('.guess-message')
-        .textContent = numberInput > randomNumber ? 'Too much!' : 'Too little!'
+      document.querySelector('.guess-message').textContent = numberInput > randomNumber ? 'Too much!' : 'Too little!';
       score--;
-      document.querySelector('.score').textContent = score;
+      scoreSize( score) ;
 
     } else {
 
-      document.querySelector('.guess-message')
-        .textContent = 'Game Over';
-      document.querySelector('.score').textContent = 0;
+      displayGuessMessage('Game Over') ;
+      scoreSize(0) ;
     }
 
   }
-
 
 });
 
 // yeniden bawlayanda
 
 document.querySelector('.again').addEventListener('click', function() {
-  document.querySelector('body').style.backgroundColor = 'black';
-  document.querySelector('.guess-message')
-    .textContent = 'start guessing';
-  document.querySelector('.question').style.width = '25rem';
-   randomNumber = Math.trunc(Math.random() * 20 + 1);
+  bodyBackgroundColor('black') ;
+  displayGuessMessage('start guessing');
+  questionSize('25rem');
+  randomNumber = Math.trunc(Math.random() * 20 + 1);
   document.querySelector('.question').textContent = '???';
   score = 20;
-  document.querySelector('.score').textContent = score;
+  scoreSize(score);
   document.querySelector('.number-input').value = '';
 });
